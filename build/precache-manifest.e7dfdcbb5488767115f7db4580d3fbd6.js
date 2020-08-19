5aec2dfe4ee85447fd4f2bbd1ce05482b9bcc9c9
vishnu-yv <47291918+vishnu-yv@users.noreply.github.com>
2020-08-19 16:59:05 +0530

    Add files via upload

---
+++ build/precache-manifest.e7dfdcbb5488767115f7db4580d3fbd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268af92571b948d4eb25b314727f68b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b0f5fa92fde15ffb14a",
+    "url": "/static/css/main.4990e117.chunk.css"
+  },
+  {
+    "revision": "e466c213f4f402d96ef3",
+    "url": "/static/js/2.d42dc289.chunk.js"
+  },
+  {
+    "revision": "fdca082fd0c4ef1db4a5b3f48c425822",
+    "url": "/static/js/2.d42dc289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b0f5fa92fde15ffb14a",
+    "url": "/static/js/main.25cbf6db.chunk.js"
+  },
+  {
+    "revision": "7feb1aeaf9fd2e6638e8",
+    "url": "/static/js/runtime-main.0266b70d.js"
+  }
+]);
